34aeec9b9d91366c14ad5ff26b3d0970eafafe59
Kevin Liu <kevinliu3931@gmail.com>
2022-05-26 00:55:17 -0400

    start

---
+++ searching/client/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Main from './components/main';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Main />
+  </React.StrictMode>
+);
+
+
